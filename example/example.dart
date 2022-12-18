aafb623d8b79013c77d5eb190879038a746f7fdd
AKORA <dragoncatm@gmail.com>
2022-12-18 17:16:05 +0100

    publish

--- example/example.dart
+++ example/example.dart
@@ -1,25 +1,7 @@
-[![pub package](https://img.shields.io/pub/v/crates_api.svg)](https://pub.dev/packages/crates_api)
-[![package publisher](https://img.shields.io/pub/publisher/crates_api.svg)](https://pub.dev/packages/crates_api/publisher)
-
-## Crates.io API Wrapper for Dart
-
-This wrapper lets you easily interact with the crates.io api.
-
-- Exposes all API types
-- Simple dart api
-- _soon_ 100% API Coverage
-
-Note: Before using this package you should read the [Crates.io Package Policies](https://crates.io/policies#crawlers).
-
-## Example
-
-See `example/example.dart`
-
-```dart
 import 'package:crates_api/crates_api.dart';
 import 'package:crates_api/types/v1/error.dart';
 
-void main() async {
+void main(List<String> arguments) async {
   var api = CratesAPI("crates_api.dart testing");
 
   var versionR = api.crate("diesel").version("2.0.2");
@@ -42,4 +24,3 @@ void main() async {
     print(e.errors.map((e) => e.detail));
   }
 }
-```
